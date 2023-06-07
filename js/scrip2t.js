927a77d1415a882f87003e530f0f31c48d954122
Khrolenko Vladyslav <VLKhrolenko@gmail.com>
2023-06-07 21:14:04 +0300

    hm3
    hm3

---
+++ js/scrip2t.js
@@ -0,0 +1,24 @@
+// Переделайте приведенный код так, чтобы в нем использовались операции +=, -=, *=, /=, ++, --.
+// Количество строк кода при этом не должно измениться. Код для переделки:
+// var num = 1;
+// num = num + 12;
+// num = num — 14;
+// num = num * 5;
+// num = num / 7;
+// num = num + 1;
+// num = num — 1;
+// alert(num);
+
+let num = 1;
+num += 12;
+console.log(num);
+num -= 14;
+console.log(num);
+num *= 5;
+console.log(num);
+num /= 7;
+console.log(num);
+num = ++num;
+console.log(num);
+num = --num;
+console.log(num)
